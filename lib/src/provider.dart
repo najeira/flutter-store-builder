437b3432e484160bbd7bdf6f80ba79fb1120d12d
najeira <najeira@gmail.com>
2020-02-18 12:05:29 +0900

    doc: StoreProvider and SubjectProvider

--- lib/src/provider.dart
+++ lib/src/provider.dart
@@ -1,22 +1,24 @@
-import 'package:flutter/widgets.dart';
-
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 
 import 'store.dart';
 
-/// Provides a [Store] to all ancestors of this Widget.
+/// Provides a [Store] to descendants of this widget.
+///
 /// This should generally be a root widget in your App.
-/// Connect to the [Store] provided by this Widget using a
-/// [StoreBuilder].
+///
+/// Connect to the [Store] provided by this widget using a [StoreBuilder] and
+/// [StoreProvider].
 class StoreProvider extends SingleChildStatelessWidget {
   const StoreProvider({
     Key key,
     @required this.store,
     @required Widget child,
-  })  : assert(store != null),
-        assert(child != null),
-        super(key: key, child: child);
+  })
+    : assert(store != null),
+      assert(child != null),
+      super(key: key, child: child);
 
   final Store store;
 
@@ -33,18 +35,30 @@ class StoreProvider extends SingleChildStatelessWidget {
   }
 }
 
+/// Provides a [StoredSubject] and its value to descendants of this widget.
+///
+/// The [SubjectProvider] gets a subject via [Provider<T>.of] from its
+/// ancestors and provides it and its value to descendants.
+///
+/// [SubjectProvider] releases the subject when this widget is disposed.
 class SubjectProvider<T> extends SingleChildStatelessWidget {
   const SubjectProvider({
     Key key,
     this.store,
     @required this.id,
     @required Widget child,
-  })  : assert(store != null),
-        assert(child != null),
-        super(key: key, child: child);
+  })
+    : assert(store != null),
+      assert(child != null),
+      super(key: key, child: child);
 
+  /// Related to this widget.
+  ///
+  /// If omitted, [SubjectProvider] will automatically find it
+  /// using [StoreProvider] and the current [BuildContext].
   final Store store;
 
+  /// Identify the subject in the [Store].
   final Object id;
 
   StoredSubject<T> _create(BuildContext context) {
